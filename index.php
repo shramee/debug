572b046d2905f2f60bc4962186ac092ec00384a8
Shramee Srivastav <shramee.srivastav@gmail.com>
2015-08-14 20:31:55 +0530

    pp_api_menu_wc_am_api_key_field

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
+			echo "<input id='api_key' name='" . $this->data_key . "[" . 'api_key' . "]' size='25' type='text' value='" . $this->options['api_key'] . "' />";
+			if ( $this->options['api_key'] ) {
+				echo "<span class='icon-pos'><img src='" . $this->plugin_url() . "pp-api/assets/images/complete.png' title='' style='padding-bottom: 4px; vertical-align: middle; margin-right:3px;' /></span>";
+			} else {
+				echo "<span class='icon-pos'><img src='" . $this->plugin_url() . "pp-api/assets/images/warn.png' title='' style='padding-bottom: 4px; vertical-align: middle; margin-right:3px;' /></span>";
+			}
 
 ?>
